0e1aae805a9a0ea7fdc379427ede979082103ebd
muningluning <satera.gkd@gmail.com>
2023-05-13 15:11:05 +0800

    update index_visitus_footer

--- components/footer.php
+++ components/footer.php
@@ -36,13 +36,13 @@
                             <?php
                                 if($contact_r['address'] != ''){
                                     echo <<<data
-                                        <p>$contact_r[address]<p>
+                                        <p style="font-size:20px">$contact_r[address]<p>
                                     data;
                                 }
                             ?>
 
                             <div class="foot">
-                                <p>C 2023 Bayangan Hotel and Beach Resort. All rights Reserved.</p>
+                                <p>© 2023 Bayangan Hotel and Beach Resort. All rights Reserved.</p>
                             </div>
                         </div>
                     </div>
@@ -50,7 +50,7 @@
                         <div class="row d-flex flex-column flex-md-row container gap-4 gap-lg-0 py-5 h-100">
                             <div class="col d-flex justify-content-start px-5 h-100">
                                 <ul class="list-unstyled plan d-flex justify-content-center flex-column gap-2">
-                                    <h2 class="plan_footer text-white">Plan your visit</h2>
+                                    <h4 class="plan_footer">PLAN YOUR VISIT</h4>
                                     <li class="nav-item"><a href="#" class="nav-link">Getting here</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">Visitor services</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">About</a></li>
@@ -64,17 +64,23 @@
                             <div class="col hotelReserve d-flex justify-content-center align-items-start gap-4 flex-column px-5 h-100 ">
                                 <div class="d-flex flex-column gap-4">
                                     <ul class="list-unstyled">
-                                        <h2 class="hotel_footer">Plan your visit</h2>
-                                        <li class="nav-item"><a href="#" class="nav-link">1111-222-333</a></li>
-                                        <li class="nav-item"><a href="#" class="nav-link">bayangan@example.com</a></li>
+                                        <h4 class="hotel_footer">HOTEL RESERVATIONS</h4>
+                                        <li class="nav-item d-flex gap-2">
+                                            <img src="../assets/telephone-fill.svg" alt="telephone_graphic">
+                                            <a href="#" class="nav-link ps-4">1111-222-333</a>
+                                        </li>
+                                        <li class="nav-item d-flex gap-2">
+                                            <img src="../assets/envelope-fill.svg" alt="envelope_graphic">
+                                            <a href="#" class="nav-link ps-4">bayangan@example.com</a>
+                                        </li>
                                     </ul>
                                     <div class="subscribe input-group">
-                                        <input type="email" placeholder="email" id="email" name="email-footer" class="form-control border-0 px-2">
-                                        <button class="btn btn_subscribe">Subscribe</button>
+                                        <input type="email" placeholder="Email Address" id="email" name="email-footer" class="form-control border-0 px-2 rounded-0">
+                                        <button class="btn btn_subscribe rounded-0">SUBSCRIBE</button>
                                     </div>
                                 </div>
                                 <div>
-                                    <p class="subscribing-info">*By subscribing, you agree to receive marketing email messages from Bayangan Hotel 
+                                    <p class="subscribing-info col-lg-8" style="font-size:10px; letter-spacing:0.15em">*By subscribing, you agree to receive marketing email messages from Bayangan Hotel 
                                     and Beach Resort at the email address provided. Unsubscribe at any time. View our 
                                     Privacy Policy and Terms of Services</p>
                                 </div>
