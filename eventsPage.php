b140fd034bc1690a47001940dc0f145566b05a05
muningluning <satera.gkd@gmail.com>
2023-05-15 16:44:38 +0800

    updatedds

--- eventsPage.php
+++ eventsPage.php
@@ -10,10 +10,10 @@ require('./admin/essentials.php')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your stay</title>
+    <title>Events</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./visitUs.css">
+    <link rel="stylesheet" href="./eventsPage.css">
 </head>
 
 <body>
@@ -25,7 +25,7 @@ require('./admin/essentials.php')
         <section class="heroStay">
             <div class="container">
                 <div class="heroStay_content d-flex justify-content-center align-items-center">
-                    <h1 class="text-white">VISIT US</h1>
+                    <h1 class="text-white">EVENTS</h1>
                 </div>
             </div>
         </section>
